756f05187d6ebd896f7cbf739c1ace8e50dfcd5b
Harsha Naik <harsha@Harshas-MacBook-Air.local>
2025-07-28 20:47:12 -0700

    updated garden name

--- src/main/java/com/example/ooad_project/HelloApplication.java
+++ src/main/java/com/example/ooad_project/HelloApplication.java
@@ -32,7 +32,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/ooad_project/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
-        stage.setTitle("DIA Organics - Smart Garden");
+        stage.setTitle("DigiGarden - Smart Garden ");
         stage.setScene(scene) ;
         stage.show();
         initializeBackgroundServices();
